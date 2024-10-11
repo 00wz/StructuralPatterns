c0e5aca880a40ab42b74cae667a47deab7b2bb15
Pavel <pedaracha@gmail.com>
2024-10-11 18:00:00 +0300

    Initial commit

---
+++ Source/StructuralPatterns/StructuralPatterns.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "StructuralPatterns.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, StructuralPatterns, "StructuralPatterns" );
---
+++ Source/StructuralPatterns/StructuralPatternsGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "StructuralPatternsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STRUCTURALPATTERNS_API AStructuralPatternsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
